59916fa00be2249f45c0e1f1e1c650ff90f7e287
Bhavya Babuta <babutabhavya@gmail.com>
2024-03-03 00:40:32 +0100

    Improve CI and add pytests

--- src/components/car/player.py
+++ src/components/car/player.py
@@ -6,10 +6,10 @@ class PlayerCar(AbstractCar):
     IMG = RED_CAR_IMAGE
     START_POS = (180, 200)
 
-    def reduce_speed(self):
+    def reduce_speed(self) -> None:
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
 
-    def bounce(self):
+    def bounce(self) -> None:
         self.vel = -self.vel
         self.move()
